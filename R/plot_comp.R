a182bc81c93c73ba48d5abdbc72881fc3b26fa61
Nils König <nils.koenig01@gmail.com>
2025-06-24 12:56:44 +0200

    Merge plot_comp_ebba into dev
    Squashed commit of the following:
    
    commit 5bad1c0892a3f5a10aa1b4ec868f8de208d2dc96
    Author: Nils König <nils.koenig01@gmail.com>
    Date:   Tue Jun 24 12:52:32 2025 +0200
    
        Revert roxygen2 version
    
    commit 1061921a5b625281c9a5ad307db608a48b9e3fc3
    Author: Nils König <nils.koenig01@gmail.com>
    Date:   Tue Jun 24 12:41:54 2025 +0200
    
        Revert plot_comp to only plot non-cumulative fesis breaks
    
    commit 938b2cccae2d096d003d3193f201c945eca0c8a4
    Author: Nils König <nils.koenig01@gmail.com>
    Date:   Tue Jun 24 11:17:32 2025 +0200
    
        Update documentation and github workflow, remove plot_comp_quick
    
    commit 61134268180399d497041fbff10cf901654bd94d
    Author: Nils König <nils.koenig01@gmail.com>
    Date:   Mon Jun 23 15:32:16 2025 +0200
    
        Add roxygen outputs
    
    commit f725aba83bc8ca9553cf5cfb4180dc774c9368d2
    Author: Nils König <nils.koenig01@gmail.com>
    Date:   Mon Jun 23 15:26:21 2025 +0200
    
        Add compare functions to namespace
    
    commit 4ec03f5ef5253f05284dcf98a634dfb7ee0171aa
    Author: Nils König <nils.koenig01@gmail.com>
    Date:   Wed Jun 18 18:51:48 2025 +0200
    
        Implement fully functional expansion of break list to panel coef table to also support iis and tis
    
    commit 51c2867402a02bb4e1203ac8fe6f16e347e3276d
    Author: Nils König <nils.koenig01@gmail.com>
    Date:   Wed Jun 18 16:21:46 2025 +0200
    
        Use gets::coef to get the coefficients faster and calculate cumulative coefficients
    
    commit 3a3e7fd7ffd76a8242a4a4dec6d500c69445ca0b
    Author: ebbam <99182071+ebbam@users.noreply.github.com>
    Date:   Mon Sep 16 16:22:35 2024 +0100
    
        plot_comp funs and example res file

---
+++ R/plot_comp.R
@@ -0,0 +1,285 @@
+###############################################################################
+###################### PLOTTING FUNCTIONS #####################################
+###############################################################################
+
+# The following contains three functions for plotting multiple models side by side. 
+# plot_comp: The first plots multiple isatpanel objects by panel cross-section or 
+#     model description using the break_uncertainty() function of getspanel
+# plot_unit: final function can plot both multiple model specifications and multiple 
+#     dependent variables for a single panel cross-section/unit for higher-dim comparisons 
+
+###############################################################################
+
+
+###############################################################################
+#' Plotting multiple isatpanel objects for comparison across units
+#'
+#' The following function takes a dataframe of isatpanel objects with associated 
+#' model names and builds a plot that allows for comparison of detected breaks 
+#' by panel cross-section. 
+#' Each line should represent a unique model specification-sample pair (no check implemented yet, user must verify).
+#' The function can only handle one outcome/dependent variable at a time.
+#'
+#' @param mod Dataframe of model objects. At minimum, requires one column with 
+#' one isatpanel object wrapped in a list per row and a second column with a unique model description.
+#' @param sign Type of break to display in plot ("all", negative ("neg"), or positive ("pos")). 
+#' @param panel Whether the results should be displayed by panel cross-section or by model ("model" or "unit")
+#' @param main_text Plot title (optional)
+#' @param blanks Boolean - whether to include models in which no break is detected for a panel cross-section.
+#' @param t_range provide time range of panel from tt:TT.
+#' @param id_list provide a character vector of unit names to isolate only certain units
+#'
+#' @return ggplot object that displays multiple isatpanel results by panel-cross section for quick comparison across multiple specifications.
+#' @export
+#' @importFrom dplyr slice %>% mutate case_when select left_join group_by summarise rename
+#' @importFrom tidyr complete
+#' @importFrom tibble tibble
+#' @importFrom ggplot2 ggplot aes geom_tile scale_fill_gradient2 scale_x_continuous scale_y_discrete facet_grid theme_bw theme labs element_text
+#'
+#' @examples
+#' library(tidyverse)
+#' library(gridExtra)
+#' library(gets)
+#' library(getspanel)
+#' res <- readRDS(here('data/standard_results_example.RDS')) 
+#' res %>% select(is, model) %>% plot_comp(t_range = 2000:2021)
+#' # Example isolating specific units
+#' res %>% select(is, model) %>% plot_comp(t_range = 2000:2021, id_list = c("Argentina", "Bulgaria", "China"))
+#'
+#'
+#'
+plot_comp <- function(mod, sign, panel = "unit", main_text = NULL, blanks = TRUE, t_range, id_list = NULL){
+  p_data <- tibble()
+  print("NOTE: This function currently only plots FESIS breaks, and only plots their absolute coefficient value (no cumulative/overlapping effects)")
+  if(nrow(mod) > 8){print(paste0("WARNING: Plotting ", nrow(mod), " models. Take care when plotting too many models."))}
+  if(nrow(mod) == 0){
+    stop("No models to plot.")
+    }
+  
+  for(r in 1:nrow(mod)){
+    print(r)
+    tmp <- mod %>% 
+      slice(r)
+    
+    coefficients <- tibble(
+      name = names(coef(tmp$is[[1]]$isatpanel.result)),
+      coef = coef(tmp$is[[1]]$isatpanel.result)
+    )
+
+    # Combine all tables in get_indicators by row and select id, time, name
+    indicators <- get_indicators(tmp$is[[1]])
+    indicators <- indicators[names(indicators) %in% "fesis"]
+    indicators_flat <- do.call(rbind, lapply(indicators, function(x) x[, c("id", "time", "name"), drop = FALSE]))
+
+    raw_breaks <- merge(indicators_flat, coefficients)
+    
+    if(missing(sign)){}
+    else if(sign == "neg"){
+      raw_breaks <- raw_breaks %>% filter(coef < 0)
+    }else if(sign == "pos"){
+      raw_breaks <- raw_breaks %>% filter(coef > 0) 
+    }
+    
+    # # Prepare indicator info before completing the time grid
+    # indicator_info <- raw_breaks %>%
+    #   mutate(
+    #     indicator_type = case_when(
+    #       grepl("^iis[0-9]+$", name) ~ "iis",
+    #       grepl("^fesis.+\\.[0-9]+$", name) ~ "fesis",
+    #       grepl("^tis.+\\.[0-9]+$", name) ~ "tis",
+    #       TRUE ~ "other"
+    #     ),
+    #     breaks = time
+    #   ) %>%
+    #   select(id, coef, indicator_type, breaks)
+    
+    # # Create a full grid of id and time
+    # full_grid <- expand.grid(
+    #   id = unique(indicator_info$id),
+    #   time = t_range
+    # )
+
+    # Old code: this only "extends" the absolute coefficient value of the last (fesis) break until the next break, this does not show the cumulative coefficient value (i.e. the net effect of all breaks)
+    p_data <- raw_breaks %>%
+      mutate(breaks = time) %>% 
+      select(id, time, coef, breaks) %>% 
+      complete(id, time = t_range) %>% 
+      group_by(id) %>% 
+      fill(coef, .direction = "down") %>% 
+      ungroup() %>% 
+      mutate(model = tmp$model) %>% 
+      rbind(p_data)
+
+    # Variant 1: this shows the coefficient value of a break at the time of the break, but also does not show any cumulative effect
+    # Note: works for iis and fesis but does not show the prolonged effect of fesis
+    # p_data <- raw_breaks %>%
+    #   mutate(breaks = time) %>%
+    #   select(id, time, coef, breaks) %>%
+    #   complete(id, time = t_range) %>%
+    #   group_by(id, time) %>%
+    #   summarise(coef = sum(coef, na.rm = TRUE), .groups = "drop") %>%
+    #   group_by(id) %>%
+    #   mutate(model = tmp$model) %>%
+    #   ungroup() %>%
+    #   rbind(p_data)
+    
+    # Variant 2: this shows the cumulative effect of all breaks, but the individual coefficient values are not distinguishable
+    # Note: only works for fesis breaks
+    # p_data <- raw_breaks %>%
+    #   mutate(breaks = time) %>%
+    #   select(id, time, coef, breaks) %>%
+    #   complete(id, time = t_range) %>%
+    #   arrange(id, time) %>%
+    #   group_by(id) %>%
+    #   mutate(coef = cumsum(replace_na(coef, 0))) %>%
+    #   ungroup() %>%
+    #   mutate(model = tmp$model) %>%
+    #   rbind(p_data)
+    
+    # This Version extends the old code idea of creating the full coefficients table from only the breaks to also support iis and tis breaks
+    # Requires some tweaking but is quite a bit faster (and potentially easier to understand) than the full indicators x panel method currently used in plot_grid and plot_indicators
+    # p_data <- full_grid %>%
+    #   left_join(indicator_info, by = "id") %>%
+    #   mutate(
+    #     active = case_when(
+    #       indicator_type == "iis"   & breaks == time ~ TRUE,
+    #       indicator_type == "fesis" & breaks <= time ~ TRUE,
+    #       indicator_type == "tis"   & breaks <= time ~ TRUE,
+    #       TRUE ~ FALSE
+    #     ),
+    #     effect = case_when(
+    #       indicator_type == "iis"   & active ~ coef,
+    #       indicator_type == "fesis" & active ~ coef,
+    #       indicator_type == "tis"   & active ~ coef * (time - breaks + 1),
+    #       TRUE ~ 0
+    #     )
+    #   ) %>%
+    #   group_by(id, time) %>%
+    #   summarise(cum_coef = sum(effect, na.rm = TRUE), .groups = "drop") %>%
+    #   mutate(model = tmp$model) %>%
+    #   rbind(p_data)
+
+    if(!is.null(id_list)){p_data <- p_data %>% filter(id %in% id_list)}
+  }
+  
+  # makes sure that empty rows are shown as well (ie. where no breaks are detected)
+  if(blanks){
+    p_data <- p_data %>%
+      complete(id, time, model)
+  }
+  
+  if(panel == "model"){
+    p_data <- p_data %>% rename(id = model, model = id)
+  }
+
+  p <- p_data %>%
+    ggplot(aes(x = time, y = model)) +
+    geom_tile(aes(fill = coef), na.rm = TRUE) +  
+    scale_fill_gradient2(na.value = NA, name = "Effect", mid = "white")+
+    scale_x_continuous(expand = c(0,0)) +
+    scale_y_discrete(expand = c(0,0), limits = rev) +
+    facet_grid(id~., scales = "free_y", space = "free") +
+    theme_bw() +
+    theme(panel.grid = element_blank(),
+          panel.border = element_rect(fill = NA),
+          strip.background = element_blank(),
+          axis.text.y = element_text(size = 6, color = "black"),
+          axis.text.x = element_text(size = 10, color = "black"),
+          strip.text.y = element_text(size = 12, angle = 0),
+          plot.caption = element_text(size = 12, hjust = 0.5),
+          legend.position = "bottom"
+    ) +
+    labs(x = NULL, y = NULL, title = main_text)
+  
+  return(p)
+}
+
+###############################################################################
+#' This function plots multiple isatpanel objects by single panel cross-section allowing for 
+#' comparison across both multiple dependent variables and multiple model specifications.
+#'
+#'
+#' @param mod Dataframe of model objects. At minimum, requires one column with 
+#' one isatpanel object wrapped in a list per row, a second column with a unique model description, 
+#' and a third column with dependent variable names.
+#' @param unit Exact string identifier of panel cross-section/unit of interest in isatpanel result.
+#' @param blanks Boolean - whether to include models in which no break is detected for a panel cross-section.
+#' @param t_range provide time range of panel from tt:TT.
+#'
+#' @return ggplot object that displays multiple isatpanel results for a single unit for quick comparison across multiple specifications.
+#' @export
+#' @importFrom tibble tibble
+#' @importFrom dplyr slice select filter mutate arrange group_by ungroup do %>%
+#' @importFrom tidyr complete fill
+#' @importFrom gridExtra grid.arrange arrangeGrob
+#' @importFrom ggplot2 ggplot aes geom_tile scale_fill_gradient2 scale_x_continuous scale_y_discrete facet_grid theme_bw theme labs
+#' @examples
+#' res <- readRDS(here('standard_results_example.RDS')) 
+#' res %>% select(is, model, dep) %>% plot_unit(t_range = 2000:2021, unit = "Peru")
+#'
+#'
+
+plot_unit <- function(mod, unit, blanks = TRUE, t_range){
+  p_data <- tibble()
+  print("NOTE: This function currently only plots FESIS breaks, and only plots their absolute coefficient value (no cumulative/overlapping effects)")
+  if(nrow(mod) > 8){print(paste0("WARNING: Plotting ", nrow(mod), " models. Take care when plotting too many models."))}
+  if(nrow(mod) == 0){
+    stop("No models to plot.")
+  }
+  if(missing(t_range)){
+    stop("Please specify t_range.")
+  }
+  
+  for(r in 1:nrow(mod)){
+    print(r)
+    tmp <- mod %>% 
+      slice(r)
+    
+    coefficients <- tibble(
+      name = names(coef(tmp$is[[1]]$isatpanel.result)),
+      coef = coef(tmp$is[[1]]$isatpanel.result)
+    )
+
+    # Combine all tables in get_indicators by row and select id, time, name
+    indicators <- get_indicators(tmp$is[[1]])
+    indicators <- indicators[names(indicators) %in% "fesis"]
+    indicators_flat <- do.call(rbind, lapply(indicators, function(x) x[, c("id", "time", "name"), drop = FALSE]))
+
+    raw_breaks <- merge(indicators_flat, coefficients)
+    
+    p_data <- raw_breaks %>%
+      mutate(breaks = time) %>% 
+      select(id, time, coef, breaks) %>% 
+      complete(id, time = t_range) %>% 
+      group_by(id) %>% 
+      fill(coef, .direction = "down") %>% 
+      ungroup() %>% 
+      mutate(model = tmp$model,
+             dep = tmp$dep) %>% 
+      rbind(p_data)
+  }
+  cmod <- p_data %>% filter(id == unit)
+  
+  if(!blanks){
+    cmod <- cmod %>% group_by(dep) %>% filter(!all(is.na(coef)))
+  }
+  if(nrow(cmod)== 0){return()}
+  # Replace NAs with 0 value so that cases in which only one model reveals an effect are displayed correctly (limitation of facet_grid)
+  cmod %>% mutate(coef = ifelse(is.na(coef), 0, coef)) %>%
+    group_by(dep) %>%
+    do(gg = {ggplot(., aes(x = time, y = model)) +
+        geom_tile(aes(fill = coef)) +
+        scale_fill_gradient2(na.value = NA, name = "Effect", oob = scales::squish) +
+        scale_x_continuous(expand = c(0,0)) +
+        scale_y_discrete(expand = c(0,0), limits = rev) +
+        facet_grid(dep~.) +
+        theme_bw() +
+        theme(panel.grid = element_blank(),
+              legend.position = "bottom",
+              strip.background = element_blank(),
+              axis.text = element_blank(), #element_text(size = 12, color = "black"),
+              strip.text.y = element_text(size = 12)) +
+        labs(x = NULL, y = NULL, title = NULL)
+    }) %>%
+    .$gg %>% arrangeGrob(grobs = ., ncol = 1) %>% grid.arrange(top = unit)
+}
