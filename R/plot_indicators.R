491f4d1d2cb59349541674cc52c60c68e8352e7a
Nils König <nils.koenig01@gmail.com>
2025-06-24 13:16:46 +0200

    Merge plot_indicators into dev
    Squashed commit of the following:
    
    commit 8f267901973cbc79d90735aaf72605bed280bfc8
    Author: Nils König <nils.koenig01@gmail.com>
    Date:   Tue Jun 24 13:13:52 2025 +0200
    
        Update with roxygen
    
    commit 82c5e6e5bc7860148d7881055c7c0c3c418504d2
    Author: Nils König <nils.koenig01@gmail.com>
    Date:   Tue Jun 17 11:52:10 2025 +0200
    
        Correct fixed effects plotting, some better descriptions
    
    commit 5f3b54f450bf82393c3dd70a658a2aca0cbb618d
    Author: Nils König <nils.koenig01@gmail.com>
    Date:   Wed Jun 11 11:43:55 2025 +0200
    
        Re-implement option to include fixed effects and some more cleaning
    
    commit 42bf1b0610cd48234864d6965c6070effb650b05
    Author: Nils König <nils.koenig01@gmail.com>
    Date:   Tue Jun 10 15:00:49 2025 +0200
    
        Cleanup and rework plot_indicators
    
    commit a1d2101e2145c50b34f39de1db0964b4b9353526
    Author: Nils König <nils.koenig01@gmail.com>
    Date:   Wed May 28 13:48:22 2025 +0200
    
        First Version of Indicator Plotting Function

---
+++ R/plot_indicators.R
@@ -0,0 +1,206 @@
+#' Plot isatpanel indicator coefficients
+#'
+#' @param x An object produced by the isatpanel function
+#' @param title Plot title. Must be a character vector.
+#' @param regex_exclude_indicators A regular expression to filter out indicators from the plot. Filter out multiple indicators using "|" (e.g. regex_exclude_indicators = "cfesis|csis". Filter out Unit or Time fixed effects indicators using "id" or "time". Default is NULL, meaning no indicators are excluded.
+#' @param zero_line Logical. If TRUE, plot a horizontal line at y = 0. Default is FALSE.
+#' @param include_fixed_effects Logical. If TRUE, fixed effects are included in the plot. Default is FALSE.
+#'
+#' @return A ggplot2 plot that plots an 'isatpanel' object and shows observed data, the fitted values, and all identified breaks and impulses.
+#' @export
+#'
+#' @importFrom ggplot2 ggplot aes geom_col geom_line facet_wrap labs theme element_blank element_rect element_line scale_color_manual scale_fill_manual guides
+#' @importFrom dplyr mutate left_join bind_rows group_by summarise case_when
+#' @importFrom tidyr pivot_longer
+#' @importFrom tibble tibble
+plot_indicators <- function(x, title = NULL, regex_exclude_indicators = NULL, zero_line = FALSE, include_fixed_effects = FALSE) {
+  panel_rows <- x$estimateddata
+  indicator_table <- x$isatpanel.result$aux$mX
+
+  # Remove input variables from the indicator table
+  indicator_table <- indicator_table[, !colnames(indicator_table) %in% names(panel_rows), drop = FALSE]
+
+  # Remove fixed effects indicators from the indicator table if not specified
+  if (!include_fixed_effects) {
+    indicator_table <- indicator_table[, !grepl("^id|^time", colnames(indicator_table)), drop = FALSE]
+  }
+
+  # Remove indicators if regex_exclude_indicators is provided
+  if (!is.null(regex_exclude_indicators)) {
+    indicator_table <- indicator_table[, !grepl(regex_exclude_indicators, colnames(indicator_table)), drop = FALSE]
+  }
+
+  # Stop if no indicators are left to plot
+  if (ncol(indicator_table) == 0) {
+    stop("No indicators found in the model. Please check your model specification.")
+  }
+
+  # Remove input variables from the panel rows and only keep id and time columns
+  panel_rows <- panel_rows[, c("id", "time"), drop = FALSE]
+  df <- cbind(panel_rows, indicator_table)
+
+  # Reshape the data to long format where each id, time, and indicator combination is a row
+  df_long <- df %>%
+    pivot_longer(
+      cols = -c(id, time),
+      names_to = "indicator",
+      values_to = "value"
+    )
+
+  # Extract the indicator type
+  df_long <- df_long %>%
+    mutate(
+      indicator_type = case_when(
+        grepl("^iis[0-9]+$", indicator) ~ "IIS",
+        grepl("^fesis.+\\.[0-9]+$", indicator) ~ "FESIS",
+        grepl("^tis.+\\.[0-9]+$", indicator) ~ "TIS",
+        grepl("^.+\\.cfesis.+\\.[0-9]+$", indicator) ~ "CFESIS",
+        grepl("^.+\\.csis[0-9]+$", indicator) ~ "CSIS",
+        grepl("^id.+$", indicator) ~ "Unit Fixed Effects",
+        grepl("^time[0-9]+$", indicator) ~ "Time Fixed Effects"
+      )
+    )
+
+  # Ensure that CSIS and CFESIS values are set to 1 if they are not zero
+  df_long <- df_long %>%
+    mutate(
+      value = ifelse(
+        indicator_type %in% c("CSIS", "CFESIS"),
+        ifelse(value != 0, 1, 0),
+        value
+      )
+    )
+
+  # Add coefficients to the data and calculate effects when indicators apply
+  coefficients <- tibble(
+    indicator = names(coef(x$isatpanel.result)),
+    coef = coef(x$isatpanel.result)
+  )
+  df_long <- df_long %>%
+    left_join(coefficients, by = "indicator") %>%
+    mutate(effect = value * coef)
+
+  if (include_fixed_effects) {
+    # Convert the time indicators to a format that includes the unit id instead of the time so they can be plotted in each facet separately
+    df_long <- df_long %>%
+      group_by(id, time, indicator_type) %>%
+      mutate(
+        indicator = ifelse(
+          indicator_type == "Time Fixed Effects",
+          paste0("time", id),
+          indicator
+        )
+      ) %>%
+      group_by(id, time, indicator, indicator_type) %>%
+      summarise(
+        value = 1,
+        coef = min(coef, na.rm = TRUE),
+        effect = sum(effect, na.rm = TRUE),
+        .groups = "drop"
+      )
+  }
+
+  # Combined effects per indicator_type per id and time
+  # test <- df_long %>%
+  #   group_by(id, time, indicator_type) %>%
+  #   summarise(effect = sum(effect, na.rm = TRUE), .groups = "drop")
+
+  # Replace 0 effects with NA for plotting
+  df_long <- df_long %>%
+    mutate(
+      effect = ifelse(effect == 0, NA, effect)
+    )
+
+  # Combine effects of all indicators per id and time
+  df_long <- df_long %>%
+    bind_rows(
+      df_long %>%
+        group_by(id, time) %>%
+        summarise(
+          indicator = "combined",
+          value = NA,
+          indicator_type = "Combined",
+          coef = NA,
+          effect = sum(effect, na.rm = TRUE),
+          .groups = "drop"
+        )
+    )
+
+  indicator_colors <- c(
+    "IIS" = "grey",
+    "CSIS" = "orange",
+    "FESIS" = "red",
+    "CFESIS" = "darkgreen",
+    "TIS" = "lightblue",
+    "Combined" = "black",
+    "Unit Fixed Effects" = "purple",
+    "Time Fixed Effects" = "violetred4"
+  )
+
+  # Create the plot
+  g <- ggplot(df_long, aes(x = time, y = effect, group = indicator, color = indicator_type, fill = indicator_type)) +
+    # Bars for IIS and CSIS
+    geom_col(
+      data = subset(df_long, indicator_type %in% c("IIS")),
+      width = 0.75,
+    ) +
+    # Lines for FESIS, CFESIS, TIS
+    geom_line(
+      data = subset(df_long, indicator_type %in% c("FESIS", "CFESIS", "TIS", "CSIS", "Time Fixed Effects", "Unit Fixed Effects")),
+      size = 0.75
+    ) +
+    # Line for Combined, set color manually to black
+    geom_line(
+      data = subset(df_long, indicator_type == "Combined"),
+      aes(color = indicator_type),
+      size = 0.75,
+      linetype = "dashed"
+    ) +
+    facet_wrap(~id, scales = "free") +
+    theme_minimal() +
+    # Customize colors for each indicator type
+    scale_color_manual(
+      values = indicator_colors,
+      name = "Indicator Type"
+    ) +
+    scale_fill_manual(
+      values = indicator_colors,
+      name = "Indicator Type"
+    ) +
+    # Customize the legend to show line and bar styles in one legend
+    guides(
+      color = guide_legend(
+        override.aes = list(
+          linetype = c(
+            setNames(
+              rep("solid", length(unique(df_long$indicator_type))),
+              unique(df_long$indicator_type)
+            ),
+            Combined = "dashed"
+          )[levels(factor(df_long$indicator_type, levels = unique(df_long$indicator_type)))],
+          shape = NA
+        )
+      )
+    ) +
+    theme_bw() +
+    theme(panel.grid = element_line(),
+          panel.border = element_rect(fill = NA),
+          strip.background = element_blank()) +
+    labs(x = NULL, y = NULL)
+
+  # Add title if provided
+  if (!is.null(title)) {
+    g <- g + labs(title = title)
+  }
+
+  # Add zero line if requested
+  if (zero_line) {
+    g <- g + geom_hline(
+      aes(yintercept = 0),
+      linewidth = 0.5,
+      color = "blue"
+    )
+  }
+
+  return(g)
+}
